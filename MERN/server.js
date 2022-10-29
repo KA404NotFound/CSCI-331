b684366531886329ad6ec289a7b55f9acbcf9ec5
KA404NotFound <kelbyabel@gmail.com>
2022-10-29 08:07:05 -0600

    mern start

---
+++ MERN/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Hello NodeJS</h1>");
+    response.write("<h3>Kelby Abel b87t476</h3>");
+    response.write("<p>Running on port 3073</p>");
+    response.end();
+}).listen(3073);
