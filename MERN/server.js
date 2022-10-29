05d31dd1ca53b3fb8ba0394e58a162b46d5d94ba
KA404NotFound <kelbyabel@gmail.com>
2022-10-29 08:46:49 -0600

    update server.js

--- MERN/server.js
+++ MERN/server.js
@@ -4,6 +4,6 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello NodeJS</h1>");
     response.write("<h3>Kelby Abel - b87t476</h3>");
-    response.write("<p>Server running on port 3073</p>");
+    response.write("<p>Running Node Server on port 3073</p>");
     response.end();
 }).listen(3073);
